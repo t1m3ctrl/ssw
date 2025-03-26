af5ed2168c00cb6317e41dbb60ab3cf38ad3eef3
t1m3ctrl <t1m3ctrl@gmail.com>
2025-03-26 18:07:09 +0700

    Добавлено логирование

--- src/main/java/org/sibsutis/comments/api/CommentsApiController.java
+++ src/main/java/org/sibsutis/comments/api/CommentsApiController.java
@@ -3,6 +3,8 @@ package org.sibsutis.comments.api;
 import org.sibsutis.comments.model.Comment;
 import org.sibsutis.comments.model.CommentInput;
 import org.sibsutis.comments.service.CommentService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,8 @@ import java.util.Optional;
 @RequestMapping("/comments")
 public class CommentsApiController implements CommentsApi {
 
+    private static final Logger logger = LoggerFactory.getLogger(CommentsApiController.class);
+
     private final CommentService commentService;
 
     @Autowired
@@ -23,28 +27,46 @@ public class CommentsApiController implements CommentsApi {
 
     @PostMapping
     public ResponseEntity<Comment> createComment(@RequestBody CommentInput input) {
-        return ResponseEntity.ok(commentService.createComment(input));
+        logger.info("Received request to create comment with input: {}", input);
+        Comment createdComment = commentService.createComment(input);
+        logger.info("Created comment: {}", createdComment);
+        return ResponseEntity.ok(createdComment);
     }
 
     @GetMapping("/{commentId}")
     public ResponseEntity<Comment> getCommentById(@PathVariable Integer commentId) {
+        logger.info("Received request to get comment by id: {}", commentId);
         Optional<Comment> comment = commentService.getCommentById(commentId);
-        return comment.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        if (comment.isPresent()) {
+            logger.info("Found comment: {}", comment.get());
+            return ResponseEntity.ok(comment.get());
+        } else {
+            logger.warn("Comment with id {} not found", commentId);
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Integer commentId) {
+        logger.info("Received request to delete comment with id: {}", commentId);
         commentService.deleteComment(commentId);
+        logger.info("Deleted comment with id: {}", commentId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/resource/{resourceId}")
     public ResponseEntity<List<Comment>> getCommentsByResource(@PathVariable Integer resourceId) {
-        return ResponseEntity.ok(commentService.getCommentsByResource(resourceId));
+        logger.info("Received request to get comments by resource id: {}", resourceId);
+        List<Comment> comments = commentService.getCommentsByResource(resourceId);
+        logger.info("Found {} comments for resource id {}", comments.size(), resourceId);
+        return ResponseEntity.ok(comments);
     }
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Comment>> getCommentsByUser(@PathVariable Integer userId) {
-        return ResponseEntity.ok(commentService.getCommentsByUser(userId));
+        logger.info("Received request to get comments by user id: {}", userId);
+        List<Comment> comments = commentService.getCommentsByUser(userId);
+        logger.info("Found {} comments for user id {}", comments.size(), userId);
+        return ResponseEntity.ok(comments);
     }
 }
